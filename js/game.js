5dd0398d727dd10c7091d48bf1bc8277ac85515d
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2024-12-08 14:50:54 -0500

    feat: :zap: game

--- js/game.js
+++ js/game.js
@@ -12,47 +12,22 @@ const SearchList = [
 
 let score = 0;
 let currentRound = 0;
-const totalRounds = 10;
+const totalRounds = 9;
 
 const resultBox = document.querySelector(".results"); //Resultados de búsqueda
 const inputBox = document.querySelector(".search-bar"); // Campo de entrada de texto
 const submitButton = document.querySelector(".submit-btn");
 const scoreBox = document.querySelector(".score-box");
 const carImage = document.querySelector(".car-image");
+const resultDiv = document.getElementById('result');
 
 function displayCarImage(round) {
-    carImage.innerHTML = `<img src="${SearchList[round].image}" alt="Imagen de ${SearchList[round].name}" style"max-width: 100%; height: auto;">`;
+    if (round < SearchList.length) {
+        carImage.innerHTML = `<img src="${SearchList[round].image}" alt="Imagen de ${SearchList[round].name}" style="max-width: 100%; height: auto;">`;
+    }
 }
 
 displayCarImage(currentRound);
-
-//Representa resultados de búsqueda
-/*const displayResults = function (result) { 
-    const resultHTML = result.map (function (cars) {return `<li onclick=selectInput(this)>${cars}</li onclick=selectInput>`;});
-
-    resultBox.innerHTML = '<ul>' + resultHTML.join ("") + '</ul>';
-}
-// Detecta la pulsación sobre la barra de búsqueda
-inputBox.onkeyup = function (e) {
-    let result = [];
-
-    const input = inputBox.value.toLowerCase();
-
-    if (input.length === 0) {
-        resultBox.innerHTML = "";
-    }
-
-    if (input.length) {
-        result = SearchList.filter ((cars) => {return cars.toLowerCase().includes(input);});
-
-        displayResults(result);
-    }
-};
-//Cuando el usuario hace clic sobre el elemento
-function selectInput(cars) {
-    inputBox.value = cars.innerText;
-    resultBox.innerHTML = "";
-}; */
 
 submitButton.onclick = function () {
     const userInput = inputBox.value.trim();
@@ -65,21 +40,29 @@ submitButton.onclick = function () {
     currentRound++;
     inputBox.value = "";
 
-    if (currentRound >= totalRounds) {
+    if (currentRound >= totalRounds || currentRound >= SearchList.length) {
         alert(`Juego terminado, tu puntuación final es ${score}`);
-        scoreBox.innerText =`Puntuación: ${score}`;
+        scoreBox.innerText =`Score: ${score}`;
         carImage.innerHTML = "";
-        if (score == 100 ) {
+
+        let image = "";
+        if (score == 90 ) {
             image = "<img src='../images/cienpuntos.png' alt='Cien Puntos' style='max-width: 100%; height: auto; margin-top: 10px;'>";
-        } else if (score >=70 && score < 100) {
+        } else if (score >=70 && score < 90) {
             image = "<img src='../images/medalla.png' alt='70 a 90' style='max-width: 100%; height: auto; margin-top: 10px;'>";
         } else if (score == 60) {
             image = "<img src='../images/puño.png' alt='60 puntos' style='max-width: 100%; height: auto; margin-top: 10px;'>";
         } else {
             image = "<img src='../images/cara.png' alt='Menos 60' style='max-width: 100%; height: auto; margin-top: 10px;'>";
         }
+
+        resultDiv.innerHTML = image;
+        resultDiv.style.display = 'block';
+
     } else {
-        scoreBox.innerText = `Puntuación: ${score} | Ronda: ${currentRound + 1}/${totalRounds}`;
+        scoreBox.innerText = `Score: ${score} | Round: ${currentRound + 1}/${totalRounds}`;
         displayCarImage(currentRound);
+        resultDiv.innerHTML = image;
+        resultDiv.style.display = 'block';
     }
 };
